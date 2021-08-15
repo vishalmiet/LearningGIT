9959dac64a419a22a5de0c2417fcaf27c8b42c15
vishalmiet <vishalmiet@gmail.com>
2021-08-15 17:28:42 +0530

    Commit 3

--- src/main/java/HelloGIT.java
+++ src/main/java/HelloGIT.java
@@ -2,5 +2,6 @@ public class HelloGIT {
     public static void main(String[] args) {
         System.out.println("Hello GIT!");
         System.out.println("Learning GIT!");
+        System.out.println("Project Folder Changed..");
     }
 }
