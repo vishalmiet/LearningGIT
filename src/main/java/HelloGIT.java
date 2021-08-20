aa39f66c2dc71c9ae1def5d05623a88e771d6510
vishalmiet <vishalmiet@gmail.com>
2021-08-20 19:14:50 +0530

    Commit 4

--- src/main/java/HelloGIT.java
+++ src/main/java/HelloGIT.java
@@ -3,5 +3,6 @@ public class HelloGIT {
         System.out.println("Hello GIT!");
         System.out.println("Learning GIT!");
         System.out.println("Project Folder Changed..");
+        System.out.println("Remove ignored files..");
     }
 }
